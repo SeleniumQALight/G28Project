f6e098c29e76ada3f634aa5e110cbab43da6df31
Allayavna <Allayavna@gmail.com>
2017-10-11 20:34:43 +0300

    first commit

--- KalininaProj/src/test/java/mainPack/AppTest.java
+++ KalininaProj/src/test/java/mainPack/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App. Test. test 44444
  */
 public class AppTest 
     extends TestCase
