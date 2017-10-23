e141c1f4af40fe96a4999ea4813a93a2305df70f
assa-test <aleks_assa@ukr.net>
2017-10-23 21:27:00 +0300

    23-10-2017
         21-26

--- BatujevskiiProj/src/test/java/mainPack/LogInWithOutPageObject.java
+++ BatujevskiiProj/src/test/java/mainPack/LogInWithOutPageObject.java
@@ -1,6 +1,8 @@
 package mainPack;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,21 +12,37 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LogInWithOutPageObject {
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        File fileFF = new File(".././drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // time-out for every command !
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
 
     @Test
     public void validLogin(){
-        File fileFF = new File(".././drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // time-out for every command !
-        driver.get("http://v3.test.itpmgroup.com/login");
-        driver.findElement(By.name("_username")).sendKeys("Student");
+
+        driver.get("http://v3.qalight.com.ua/login");
+        driver.findElement(By.name("_username")).sendKeys("student");
         driver.findElement(By.xpath(".//*[@name='_password']")).sendKeys("909090");
         driver.findElement(By.tagName("button")).click();
-        Assert.assertTrue(driver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']"))
+
+        Assert.assertTrue("Message if false !",driver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']"))
                 .isDisplayed());
-        driver.quit();
+        // проверка
+
+
     }
+
 /*
     @Test
     public void unvalidLogin() {
