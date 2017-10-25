0e621b0798678e923c52503fe533215f1d9ad8a4
VovkA <wowk2002@ukr.net>
2017-10-25 08:22:23 +0300

    Add unvalidlogin test

--- VovkProj/src/main/java/mainPack/LogInWithOutPageObject.java
+++ VovkProj/src/main/java/mainPack/LogInWithOutPageObject.java
@@ -1,6 +1,8 @@
 package mainPack;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,37 +12,39 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class LogInWithOutPageObject {
+    WebDriver driver;
 
+    @Before
+    public void setUp() {
+        File fileFF = new File(".././drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown () {
+        driver.quit();
+    }
 
 
     @Test
     public void validLogin(){
-        File fileFF = new File(".././drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://v3.test.itpmgroup.com/login");
         driver.findElement(By.name("_username")).sendKeys("Student");
         driver.findElement(By.xpath(".//*[@name='_password']")).sendKeys("909090");
         driver.findElement(By.tagName("button")).click();
         Assert.assertTrue(driver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']"))
                 .isDisplayed());
-        driver.quit();
     }
 
     @Test
     public void unvalidLogIn(){
-        File fileFF = new File(".././drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://v3.test.itpmgroup.com/login");
         driver.findElement(By.name("_username")).sendKeys("badlogin");
         driver.findElement(By.xpath(".//*[@name='_password']")).sendKeys("badparol");
         driver.findElement(By.tagName("button")).click();
         Assert.assertTrue(driver.findElement(By.xpath(".//b[contains(text(), 'Учет запчастей')]"))
                 .isDisplayed());
-        driver.quit();
-
     }
 }
