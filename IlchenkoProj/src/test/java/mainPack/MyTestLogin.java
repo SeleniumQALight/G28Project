d9d7c4b4d2d8f8b5d68c4dc6dfa4f40d0f3808ec
oleksandr-ilchenko <aleksandr_ilchenko@yahoo.com>
2017-10-25 17:39:12 +0200

    25.10.2017. DZ_Changes_2

--- IlchenkoProj/src/test/java/mainPack/MyTestLogin.java
+++ IlchenkoProj/src/test/java/mainPack/MyTestLogin.java
@@ -33,8 +33,8 @@ public class MyTestLogin {
 
     }
 
-    public class MyTestLoginNoValid {
-        WebDriver driver = new ChromeDriver();
+//    public class MyTestLoginNoValid {
+//        WebDriver driver = new ChromeDriver();
 
         @Test
         public void MyTestLoginNoValid() {
@@ -68,7 +68,7 @@ public class MyTestLogin {
 
 
         }
-    }
+
 
 
 
