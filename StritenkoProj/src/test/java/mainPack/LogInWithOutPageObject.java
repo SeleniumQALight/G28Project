cdcbeebfc2294a7fd7152f14daf50fab67f8c6e4
Marina <stritenko@mail.ru>
2017-10-25 08:04:02 +0300

    valid login + @before and @after

--- StritenkoProj/src/test/java/mainPack/LogInWithOutPageObject.java
+++ StritenkoProj/src/test/java/mainPack/LogInWithOutPageObject.java
@@ -1,6 +1,8 @@
 package mainPack;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,24 +15,31 @@ import java.util.concurrent.TimeUnit;
  * Created by Marina on 10/21/2017.
  */
 public class LogInWithOutPageObject {
+    WebDriver driver;
 
-
-
-   WebDriver driver = new ChromeDriver();
-    @Test
-
-    public void validLogIn(){
+    @Before
+    public void setUp() {
         File fileFF = new File(".././drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+    @Test
+    public void validLogin() {
+
         driver.get("http://v3.test.itpmgroup.com/login");
-        driver.findElement(By.id("_name")).sendKeys("Student");
+        driver.findElement(By.name("_username")).sendKeys("Student");
         driver.findElement(By.xpath(".//*[@name='_password']")).sendKeys("909090");
         driver.findElement(By.tagName("button")).click();
-        Assert.assertTrue(" ",driver.findElement(By.xpath("//div[@class='e']//img[@class='img-circle']")).isDisplayed());
-
-        driver.quit();
-
+        Assert.assertTrue("message", driver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']"))
+                .isDisplayed());
 
     }
 }
