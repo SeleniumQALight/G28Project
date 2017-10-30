b79b47395eedd85e5370f503136addf6f691131b
alex00839 <prokofiev80@gmail.com>
2017-10-30 21:37:02 +0200

    Tenth commit

--- ProkofievProj/src/main/java/Pages/LoginPage.java
+++ ProkofievProj/src/main/java/Pages/LoginPage.java
@@ -1,36 +1,62 @@
 package Pages;
 
 import junit.framework.Assert;
+import libs.ActionsWithOurWebElements;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     WebDriver webDriver;
     Logger logger;
+    ActionsWithOurWebElements actionsWithOurWebElements;
+
+    @FindBy(name = "_username")
+    WebElement inputLogin;
+
+    @FindBy(name = "_password")
+    WebElement inputPassWord;
+
+    @FindBy(tagName = "button")
+    WebElement buttonLogIn;
 
     public LoginPage(WebDriver webDriver) {
         this.webDriver = webDriver;
         logger = Logger.getLogger(getClass());
+        actionsWithOurWebElements = new ActionsWithOurWebElements(webDriver);
+        PageFactory.initElements(webDriver, this);
     }
 
-    public void openPageLogin(){
-        try{
+    public void openPageLogin() {
+        try {
             webDriver.get("http://v3.test.itpmgroup.com/login");
             logger.info("Page login was opened");
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Can not open url");
             Assert.fail("Can not open url");
         }
     }
 
     public void enterLoginIntoInput(String login) {
-        try{
-            webDriver.findElement(By.name("_username")).sendKeys(login);
-            logger.info(login + " was entered in to login Input");
-        }catch (Exception e){
-            logger.error("Can not work with input");
-            Assert.fail("Can not work with input");
-        }
+//        try{
+//            inputLogin.clear();
+//            inputLogin.sendKeys(login);
+//            logger.info(login + " was entered in to login Input");
+//        }catch (Exception e){
+//            logger.error("Can not work with input");
+//            Assert.fail("Can not work with input");
+//        }
+        actionsWithOurWebElements.enterTextInToInput(inputLogin, login);
+    }
+
+    public void enterPassInToInputPass(String pass) {
+        actionsWithOurWebElements.enterTextInToInput(inputPassWord, pass);
+    }
+
+    public void clickOnButtonLogIn() {
+        actionsWithOurWebElements.clickOnWebElement(buttonLogIn);
     }
 }
