d6db3aaadb5b95a111503ebda598fceca70f98ea
alex00839 <prokofiev80@gmail.com>
2017-10-30 21:36:48 +0200

    Tenth commit

--- ProkofievProj/src/test/java/logInTest/LogIn.java
+++ ProkofievProj/src/test/java/logInTest/LogIn.java
@@ -1,14 +1,16 @@
 package logInTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import parentTest.ParentTest;
 
 public class LogIn extends ParentTest {
     @Test
-    public void validLogIn(){
+    public void validLogIn() {
         loginPage.openPageLogin();
         loginPage.enterLoginIntoInput("Student");
-
-
+        loginPage.enterPassInToInputPass("909090");
+        loginPage.clickOnButtonLogIn();
+        Assert.assertTrue("Avatar is not present", homePage.isAvatarPresent());
     }
 }
