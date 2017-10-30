4e70c27c11ea707683ce33ee5599cec87c6e4abc
Aleksandr <sane4ek1990@gmil.com>
2017-10-30 21:47:55 +0200

    коментарии

---
+++ FedorenkoProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by Aleksandr on 30.10.2017.
+ */
+public class HomePage extends ParentPage{
+    @FindBy (xpath = ".//div[@class='pull-left image']//img[@class='img-circle']")
+    WebElement avatar;
+
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public boolean isAvatarPresent() {
+        return actionsWithOurWebElements.isElementPresent(avatar);
+    }
+}
