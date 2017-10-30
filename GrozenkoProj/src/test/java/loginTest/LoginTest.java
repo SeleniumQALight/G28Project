3a76fd5edeb8bd27dd17ad36ba0f0786a884e228
muks30 <7567788@gmail.com>
2017-10-30 19:29:14 +0200

    commit LoginPage-LoginTest-ParenTest

--- GrozenkoProj/src/test/java/loginTest/LoginTest.java
+++ GrozenkoProj/src/test/java/loginTest/LoginTest.java
@@ -1,11 +1,11 @@
-package loginTest; // Тесты, тест кейсы
+package loginTest; // для написания команд для удобного чтения обычн людьми
 
 import org.junit.Test;
 import parentTest.ParentTest;
 
 public class LoginTest extends ParentTest {
 
-    @Test
+    @Test // Тест кейс
     public void validLogIn (){
         loginPage.openPageLogin();
         loginPage.enterLogInIntoInputLogin("Student");
