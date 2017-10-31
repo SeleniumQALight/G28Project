be87a5d7e859787867d772391b71956e13731cff
muks30 <7567788@gmail.com>
2017-10-31 09:58:34 +0200

    commit ParentTest

--- GrozenkoProj/src/test/java/parentTest/ParentTest.java
+++ GrozenkoProj/src/test/java/parentTest/ParentTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
@@ -14,8 +15,9 @@ import java.util.concurrent.TimeUnit;
 public class ParentTest {
 
     WebDriver driver;
-     //переменная
+    //переменная
     public LoginPage loginPage;
+    public HomePage homePage;
 
     //precondition
     // before будет запускаться предкаждым тестом
@@ -32,7 +34,8 @@ public class ParentTest {
         // неявное ожидание , команда будет выполнятся в течении 10сек и выполнять через  0,5 сек
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        loginPage = new LoginPage (driver);
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
     }
 
 
