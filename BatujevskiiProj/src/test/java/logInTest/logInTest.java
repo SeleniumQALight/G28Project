cbcc04179d68dc7263f6d324176b4708f9de4eb8
assa-test <aleks_assa@ukr.net>
2017-10-31 11:38:05 +0200

    test page object

--- BatujevskiiProj/src/test/java/logInTest/logInTest.java
+++ BatujevskiiProj/src/test/java/logInTest/logInTest.java
@@ -1,14 +1,17 @@
 package logInTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import parentTest.ParentTest;
 
 public class logInTest extends ParentTest {
     @Test
-    public void validLogIn(){
+    public void validLogIn() {
         loginPage.openPageLogin();
-        loginPage.enterLogInToInputLogin("student");
-        loginPage.clickButton();
+        loginPage.enterLogInIntoInputLogin("student");
+        loginPage.enterPassInToInputPass("909090");
+        loginPage.clickOnButtonLogin();
+        Assert.assertTrue("Avatar is not present", homePage.isAvatarPresent());
     }
 
 
