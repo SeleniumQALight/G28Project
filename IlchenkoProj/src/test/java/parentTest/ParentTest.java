c549d49d60876caba3320698ad5eb008cd421faf
oleksandr-ilchenko <aleksandr_ilchenko@yahoo.com>
2017-11-01 21:51:53 +0200

    ClassWorkScreenShot 01.11.2017

--- IlchenkoProj/src/test/java/parentTest/ParentTest.java
+++ IlchenkoProj/src/test/java/parentTest/ParentTest.java
@@ -1,7 +1,11 @@
 package parentTest;
 
+import libs.Utils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
@@ -10,6 +14,8 @@ import pages.LoginPage;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * Created by user on 25.10.2017.
  */
@@ -17,20 +23,43 @@ public class ParentTest {
     WebDriver driver;
     public LoginPage loginPage;
     public HomePage homePage;
+    private Utils utils = new Utils();
+    private boolean isTestPass = false;
+    private String pathToSreenShot;
+
+
+    @Rule
+    public TestName testName = new TestName();
 
 
     @Before
     public void setUp(){
         File fileFF = new File(".././drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
+        pathToSreenShot = "..\\IlchenkoProj\\target\\screenshot\\" + this.getClass().getPackage().getName()
+                + "\\" + this.getClass().getSimpleName() + "\\" + this.testName.getMethodName() + ".jpg";
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         loginPage = new LoginPage(driver);
         homePage = new HomePage(driver);
+
     }
 
     @After
     public void  tearDown(){
+        if (!isTestPass) {
+            utils.screenShot(pathToSreenShot,driver);
+        }
+
         driver.quit();
+    }
+
+    protected void checkAcceptanceCriteria(String message, boolean actual, boolean expected){
+        if (actual!= expected){
+         utils.screenShot(pathToSreenShot, driver);
+        }
+        Assert.assertThat(message,actual,is(expected));
+        isTestPass = true;
+
     }
 }
