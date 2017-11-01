99e08eec385e5feda337ac26466287984409bab3
assa-test <aleks_assa@ukr.net>
2017-11-01 19:54:07 +0200

    25-10-2017
         21-33

--- BatujevskiiProj/src/test/java/parentTest/ParentTest.java
+++ BatujevskiiProj/src/test/java/parentTest/ParentTest.java
@@ -1,16 +1,22 @@
 package parentTest;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class ParentTest {
     WebDriver driver;
     public LoginPage loginPage;  // peremennaia
+    public HomePage homePage;
 
     @Before
     public void setUp(){
@@ -19,12 +25,21 @@ public class ParentTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // time-out for every command !
         loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
 
     }
 
     @After
     public void tearDown() {
         driver.quit();
+    }
+
+
+
+    protected void checkAcceptanceCriteria(String message, boolean actual, boolean expected ){
+    //асерт выносим и заворачиваем в обертку -
+        Assert.assertThat(message, actual, is(expected));
+
     }
 
 
