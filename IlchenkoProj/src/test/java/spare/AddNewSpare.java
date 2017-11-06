b56c6bda5bd77c18f0461721b43252ea4e5ffdef
oleksandr-ilchenko <aleksandr_ilchenko@yahoo.com>
2017-11-06 21:35:12 +0200

    ClassWorkSparePage 06.11.2017

---
+++ IlchenkoProj/src/test/java/spare/AddNewSpare.java
@@ -0,0 +1,24 @@
+package spare;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+/**
+ * Created by user on 06.11.2017.
+ */
+public class AddNewSpare extends ParentTest {
+    final private  String TEST_SPARE = "Spare1";
+
+    @Test
+    public void addNewSpare(){
+        loginPage.loginUser("Student", "909090");
+        homePage.checkTitle("Учет запчастей");
+        homePage.checkZagolovok("Главная");
+        homePage.clickOnMenuDictionary();
+        homePage.clickOnSubMenuSpare();
+        sparePage.checkZagolovok("Запчасти Список");
+
+
+
+    }
+}
