a06f6a45aacf1bcb385fb9122c2941b76ca6cbef
muks30 <7567788@gmail.com>
2017-11-06 21:35:25 +0200

    commit LoginTest

--- GrozenkoProj/src/test/java/loginTest/LoginTest.java
+++ GrozenkoProj/src/test/java/loginTest/LoginTest.java
@@ -19,7 +19,16 @@ public class LoginTest extends ParentTest {
         // будет выведено сообщение "Avatar is not present"
         checkAcceptanceCriteries("Avatar is not present",
                 homePage.isAvatarPresent(), true); // если поменять на false - получим скриншот
+
+        checkAcceptanceCriteries("Title is not excepted",
+                homePage.getTitle(),"Учет запчастей");
+
+    //Calendar - спрашиваем дату и время
+    // SimpleFormat -  выбираем формат отображения времени и даты
     }
+
+
+
 
 
 }
