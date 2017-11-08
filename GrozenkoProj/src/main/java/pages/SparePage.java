df1aade8278c8102ae2787c211412b45ff5643a2
muks30 <7567788@gmail.com>
2017-11-08 21:48:34 +0200

    commit SparePage

--- GrozenkoProj/src/main/java/pages/SparePage.java
+++ GrozenkoProj/src/main/java/pages/SparePage.java
@@ -1,11 +1,29 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class SparePage extends ParentPage{
+public class SparePage extends ParentPage {
+
+    // перейти на страницу по линку
+    @FindBy(xpath = ".//*[@href='http://v3.test.itpmgroup.com/dictionary/spares/edit']")
+    private WebElement buttonPlus;
 
     // конструктор
     public SparePage(WebDriver webDriver) {
         super(webDriver);
     }
+
+    public void clickOnPlusButton() {
+
+        actionsWithOurWebElements.clickOnWebElement(buttonPlus);
+    }
+
+    public boolean isSpareInList(String nameOfSpare) {
+        return actionsWithOurWebElements.isElementPresent(
+                ".//*[text()='" + nameOfSpare + "']");
+    }
+
+
 }
