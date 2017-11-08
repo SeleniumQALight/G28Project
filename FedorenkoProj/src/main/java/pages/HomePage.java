44316bd3fb7379f2de74a589d2dd01f1bff85e63
Aleksandr <sane4ek1990@gmil.com>
2017-11-08 12:27:14 +0200

    copy

--- FedorenkoProj/src/main/java/pages/HomePage.java
+++ FedorenkoProj/src/main/java/pages/HomePage.java
@@ -4,18 +4,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-/**
- * Created by Aleksandr on 30.10.2017.
- */
+import javax.xml.ws.WebEndpoint;
+
 public class HomePage extends ParentPage{
-    @FindBy (xpath = ".//div[@class='pull-left image']//img[@class='img-circle']")
+    @FindBy(xpath = ".//div[@class='pull-left image']//img[@class='img-circle']")
     WebElement avatar;
+
+    @FindBy(xpath = ".//*[@id='dictionary']/a")
+    private WebElement menuDictionary;
+
+    @FindBy(id = "spares")
+    private WebElement subMenuSpare;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
-    public boolean isAvatarPresent() {
+    public boolean isAvatarPresent(){
         return actionsWithOurWebElements.isElementPresent(avatar);
+    }
+
+    public void clickOnMenuDictionary() {
+        actionsWithOurWebElements.clickOnWebElement(menuDictionary);
+    }
+
+    public void clickOnSubMenuSpare() {
+        actionsWithOurWebElements.clickOnWebElement(subMenuSpare);
     }
 }
