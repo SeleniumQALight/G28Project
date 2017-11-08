a3c44f062e00cce26a39f41bad518016477f803e
eantonenko <slepczova.elena@gmail.com>
2017-11-08 23:08:34 +0200

    Class08112017 and DeleteSpare HomeWork commit

--- AntonenkoProj/src/test/java/spareTest/AddNewSpare.java
+++ AntonenkoProj/src/test/java/spareTest/AddNewSpare.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import parentTest.ParentTest;
 
 public class AddNewSpare extends ParentTest {
-    final private String TEST_SPARE = "Spare1";
+    final private String TEST_SPARE = "SpareAntonenko";
 
     @Test
     public void addNewSpare(){
@@ -15,6 +15,34 @@ public class AddNewSpare extends ParentTest {
         homePage.clickOnMenuDictionary();
         homePage.clickOnSubMenuSpare();
         sparePage.checkZagolovok("Запчасти Список");
+        sparePage.clickOnPlysButton();
+        editNewSparePage.checkZagolovok("Словарь");
+        editNewSparePage.enterTextInToInputSpareName(TEST_SPARE);
+        editNewSparePage.selectTextInDropDownTypeOfSpare("Датчики");
+        editNewSparePage.clickOnAddButton();
+        sparePage.checkZagolovok("Запчасти Список");
+        checkAcceptanceCriteria(TEST_SPARE + "Can not fined spare in list", sparePage.isSpareInList(TEST_SPARE),true);
+    }
 
+    @Test
+    public void deleteNewSpare(){
+        loginPage.loginUser("Student","909090");
+        homePage.checkTitle("Учет запчастей");
+        homePage.checkZagolovok("Главная");
+        homePage.clickOnMenuDictionary();
+        homePage.clickOnSubMenuSpare();
+        sparePage.checkZagolovok("Запчасти Список");
+        sparePage.clickOnPlysButton();
+        editNewSparePage.checkZagolovok("Словарь");
+        editNewSparePage.enterTextInToInputSpareName(TEST_SPARE);
+        editNewSparePage.selectTextInDropDownTypeOfSpare("Датчики");
+        editNewSparePage.clickOnAddButton();
+        sparePage.checkZagolovok("Запчасти Список");
+        checkAcceptanceCriteria(TEST_SPARE + "Can not fined spare in list", sparePage.isSpareInList(TEST_SPARE),true);
+        sparePage.clickOnSpareInList(TEST_SPARE);
+        editNewSparePage.checkZagolovok("Словарь");
+        editNewSparePage.clickOnDeleteButton();
+        sparePage.checkZagolovok("Запчасти Список");
+        checkAcceptanceCriteria(TEST_SPARE + "Fined spare in list", sparePage.isSpareInList(TEST_SPARE),false);
     }
 }
