4dcf4a01f2f614a8ba6cfc5762db8a275ecb76a6
o.bilous <bilousstudy@gmail.com>
2017-11-12 22:45:58 +0200

    fixed logout test

--- BilousProj/src/test/java/dictionaryTest/apparatDictionary/AddDictionaryAppart.java
+++ BilousProj/src/test/java/dictionaryTest/apparatDictionary/AddDictionaryAppart.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import parentTest.ParentTest;
 
 public class AddDictionaryAppart extends ParentTest {
+    final private String apparatNumber = "51120172";
+    final private String apparatComment = "BilousTestComment";
+
     @Test
     public void addNewDictApparat() {
         loginPage.loginUser("Student","909090");
@@ -13,8 +16,8 @@ public class AddDictionaryAppart extends ParentTest {
         homePage.selectOnDictionaryMenuItemApparat();
 
         apparatPage.clickOnAddBtn();
-        apparatPage.enterInIntoInputApparatNumber("51120172");
-        apparatPage.enterInIntoInputApparatComment("BilousTestComment");
+        apparatPage.enterInIntoInputApparatNumber(apparatNumber);
+        apparatPage.enterInIntoInputApparatComment(apparatComment);
         apparatPage.clickCreateButton();
         checkAcceptanceCriteria("Added Dictionary is not present",
                 apparatPage.isAddedDictionaryPresent(), true);
