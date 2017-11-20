c44a51d0d0492eba082c553c9e130dd97602009b
alex00839 <prokofiev80@gmail.com>
2017-11-20 21:15:25 +0200

    Sixteenth commit

---
+++ ProkofievProj/src/test/java/testDB/TestDataBase.java
@@ -0,0 +1,33 @@
+package testDB;
+
+import libs.Database;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class TestDataBase {
+    Logger logger = Logger.getLogger(getClass());
+    Database database;
+
+    @Before
+    public void setUp() throws SQLException, IOException, ClassNotFoundException {
+        database = new Database("MySQL_PADB_DB", "MySQL");
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        database.quit();
+    }
+
+    @Test
+    public void testWithDataBase() throws SQLException {
+        List<Map<String,String>> testData = database.selectTable("select * from seleniumTable");
+        logger.info(testData);
+    }
+}
