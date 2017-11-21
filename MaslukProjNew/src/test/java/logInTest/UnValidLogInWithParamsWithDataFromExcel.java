ec47e27081bce5f5ee5e492ce06a20d1f433253d
Taras Radulenko <Taras_Radulenko@epam.com>
2017-11-21 15:44:27 +0200

    KalininaProjNew and MaslukProjNew were added

---
+++ MaslukProjNew/src/test/java/logInTest/UnValidLogInWithParamsWithDataFromExcel.java
@@ -0,0 +1,49 @@
+package logInTest;
+
+import libs.ConfigData;
+import libs.SpreadsheetData;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import parentTest.ParentTest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class UnValidLogInWithParamsWithDataFromExcel extends ParentTest {
+    String login, pass;
+
+    public UnValidLogInWithParamsWithDataFromExcel(String browser, String login, String pass) {
+        super(browser);
+        this.login = login;
+        this.pass = pass;
+    }
+
+    //    @Parameterized.Parameters
+//    public static Collection testDData(){
+//        return Arrays.asList(new Object[][]{
+//                {"chrome","Student","906090"},
+//                {"iedriver","Student","906090"},
+//                {"chrome","Student","909090"},
+//                {"chrome","Student","90909"}
+//        });
+//    }
+    @Parameterized.Parameters
+    public static Collection testData() throws IOException {
+        InputStream spreadsheet = new FileInputStream(ConfigData.getCfgValue("DATA_FILE_PATH") + "testDataSuit.xls");
+        return new SpreadsheetData(spreadsheet, "InvalidLogOn").getData();      //2й параметр - указываем название листа в екселе
+    }
+
+
+    @Test
+    public void unvalidLogIn() {
+        loginPage.loginUser(login, pass);
+        checkAcceptanceCriteria("Title not match"
+                , loginPage.getTitle(),
+                "Account of spare:Авторизация");
+    }
+}
---
+++ MaslukProjNew/src/test/java/mainPack/AppTest.java
@@ -0,0 +1,41 @@
+package mainPack;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App. Test3344
+ * HomeWork - create  new branch - muks30
+ * Create second commit and push - muks30
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case 4545
+     *
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
